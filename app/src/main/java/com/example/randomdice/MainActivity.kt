66964889d0bbee2d742ee15a6ba197c39c38e2b2
LennyRBriones <lennyrbriones@outlook.com>
2024-01-09 01:58:40 -0600

    Finished. Toast, when, random, comments, lateinit val, tools & drawable used

--- app/src/main/java/com/example/randomdice/MainActivity.kt
+++ app/src/main/java/com/example/randomdice/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.randomdice
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+// lateinit necesario para cargar las variables desde antes y hacer menos consumo
+
+    lateinit var diceImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollbutton: Button = findViewById(R.id.rollbutton)
+        rollbutton.setOnClickListener {
+            rollDice()
+        }
+
+        diceImage = findViewById(R.id.dice_image)
+}
+
+    private fun rollDice() {
+
+        val randomInt = Random().nextInt( 6) + 1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
